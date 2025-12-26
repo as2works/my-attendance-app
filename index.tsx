8a565fbc44587b6515fbbe348964d3cca25cca86
as2works <kenta.asou@as2works.net>
2025-12-26 19:04:03 +0900

    Fix project structure and build config

---
+++ index.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Amplify } from 'aws-amplify';
+import outputs from './amplify_outputs.json';
+
+// AWS Amplify Configuration
+Amplify.configure(outputs);
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
